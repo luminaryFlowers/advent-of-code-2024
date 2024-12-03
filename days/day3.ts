748ce1d390438368343f2f592077b25ba188e4d5
luminaryFlowers <71102109+luminaryFlowers@users.noreply.github.com>
2024-12-03 00:59:01 -0800

    day 3 => the interaction between regex and newline characters needs to be studied and i need something terrible to happen

---
+++ days/day3.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from 'fs';
+
+const input: string = readFileSync('./days/day3input.txt', 'utf-8');
+
+/* PART 1 */
+const totalFinder = (input: string) => {
+    const regex = /(mul\([0-9]+,[0-9]+\))/g;
+    let res = regex.exec(input);
+
+    let total = 0;
+    while (res) {
+        if (res) {
+            let test = res[0].split(',');
+            total += parseInt(test[0].slice(4), 10) * parseInt(test[1], 10);
+        }
+        res = regex.exec(input);
+    }
+
+    return total;
+}
+
+console.log(totalFinder(input));
+
+/* PART 2 */
+const regex = /(((do\(\)).*?don't\(\))|do\(\).*|^.*?don't\(\))/g;
+let filteredInput = input.replace(/(\r\n|\n|\r)/gm, "")
+let res = regex.exec(filteredInput);
+
+let total = 0;
+while (res) {
+    total += totalFinder(res[0]);
+    res = regex.exec(filteredInput);
+}
+
+console.log(total);
