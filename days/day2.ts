1a3cf22582b398bff5bd17e1773ed03f7d8d309c
luminaryFlowers <71102109+luminaryFlowers@users.noreply.github.com>
2024-12-03 20:45:55 -0800

    made day2 better

--- days/day2.ts
+++ days/day2.ts
@@ -36,22 +36,41 @@ for (const line of lines) {
 console.log(safe);
 
 /* PART 2 */
-// i am not proud of this one! :blush:
 let safe2 = 0;
+
+// i'm allowed a little brute forcing as a treat
+const signFinder = (report: number[]) => {
+    let holder = 0;
+    for (let i = 1; i < report.length; i++) {
+        holder += report[i] - report[i - 1];
+    }
+    return Math.sign(holder);
+}
+
+const brokenIndex = (report: number[], signer) => {
+    // makes all lists ascending
+    const r: number[] = report.map(e => e * signer);
+
+    let prev = r[0];
+    for (let i = 1; i < r.length; i++) {
+        if (!(r[i] > prev && r[i] <= prev + 3)) {
+            return i - 1;
+        }
+        prev = r[i];
+    }
+
+    return -1;
+}
 
 for (const line of lines) {
     const reportString: string[] = line.split(" ");
     const report: number[] = reportString.map(e => parseInt(e));
+    const signer = signFinder(report);
 
-    if (isSafe(report)) safe2++;
-    else {
-        for (let i = 0; i < report.length; i++) {
-            if (isSafe(report.filter((_, index) => i != index))) {
-                safe2++;
-                break;
-            }
-        }
-    }
+    const breaker = brokenIndex(report, signer);
+
+    if (breaker < 0 || brokenIndex(report.filter((_, i) => breaker != i), signer) < 0 || brokenIndex(report.filter((_, i) => breaker + 1 != i), signer) < 0)
+        safe2++;
 }
 
 console.log(safe2);
